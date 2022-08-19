016158a1f906f59c4967d393215802b30a378acb
Joe Dodd <joehdodd@gmail.com>
2022-08-19 09:00:47 -0400

    Style contributors list and list items.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import { useState, useEffect } from "react";
+import styled from "styled-components";
 
-/*
- * https://docs.github.com/en/rest/repos/repos#list-repository-contributors
- */
+const ContributorAppWrapper = styled.div`
+  padding: 4%;
+`;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  @media screen and (min-width: 768px) {
+    padding: 0 80px;
+  }
+  @media screen and (min-width: 1024px) {
+    padding: 0 160px;
+  }
+  @media screen and (min-width: 1200px) {
+    padding: 0 240px;
+  }
+  @media screen and (min-width: 1440px) {
+    padding: 0 320px;
+  }
+`;
+
+const ContributorContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(auto-fit, 1fr);
+  grid-gap: 24px;
+`;
+
+const Contributor = styled.div`
+  display: grid;
+  grid-template-columns: 0.25fr 1.75fr;
+  grid-gap: 4px;
+  align-items: center;
+  padding: 4px 8px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  :hover {
+    background-color: #f0f2f5;
+  }
+  img {
+    width: 100px;
+    border-radius: 4px;
+  }
+`;
+
+const ContributorName = styled.span`
+  font-size: 1.25rem;
+  font-weight: bold;
+  color: #402c56;
+`;
 
 function RepoContributorApp() {
   const [contributors, setContributors] = useState([]);
@@ -18,15 +67,18 @@ function RepoContributorApp() {
       });
   }, []);
   return (
-    <div>
-      <h1>JOIN US</h1>
-      {contributors.map((contributor) => (
-        <div key={contributor.id}>
-          <img src={contributor.avatar_url} alt={contributor.login} />
-          <p>{contributor.login}</p>
-        </div>
-      ))}
-    </div>
+    <ContributorAppWrapper>
+      <Container>
+        <ContributorContainer>
+          {contributors.map((contributor) => (
+            <Contributor key={contributor.id}>
+              <img src={contributor.avatar_url} alt={contributor.login} />
+              <ContributorName>{contributor.login}</ContributorName>
+            </Contributor>
+          ))}
+        </ContributorContainer>
+      </Container>
+    </ContributorAppWrapper>
   );
 }
 
